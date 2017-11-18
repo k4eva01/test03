bc8690998c5b5a8f35ae68fe9521a0bf0737a34e
kingksin <k4eva1123@gmail.com>
2017-11-18 16:43:34 +0900

    Test03_01

---
+++ src/test03/Test03.java
@@ -0,0 +1,15 @@
+package test03;
+
+public class Test03 {
+	public static void main(String[] args) {
+	
+		
+		String id = "testid";
+		String passwd ="1234";
+		if(id == "tesid") {
+			if(passwd == "1234") {
+				System.out.println("로그인이 성공하였습니다.");
+			}
+		}
+	}
+}
